3cd7371766015cd907a43ab0a569c4cbf2689d68
Saifur Rahman <ai22m055@technikum-wien.at>
2023-03-16 16:31:55 +0100

    add Window Event

--- KitForge/src/KitForge/Application.h
+++ KitForge/src/KitForge/Application.h
@@ -2,6 +2,7 @@
 
 #include "Core.h"
 #include "Events/Event.h"
+#include "KitForge/Events/ApplicationEvent.h"
 #include "Window.h"
 
 namespace KitForge {
@@ -12,6 +13,11 @@ namespace KitForge {
 		virtual ~Application();
 
 		void Run();
+
+		void OnEvent(Event& e);
+
+	private:
+		bool OnWindowClose(WindowCloseEvent& e);
 
 	private:
 		std::unique_ptr<Window> m_Window;
